6da9440894c834d71e63ae9d31e1abda148ba400
mohit620 <patilmohit25@gmail.com>
2021-01-06 18:06:45 +0530

    new commit

--- testPassword.py
+++ testPassword.py
@@ -4,6 +4,12 @@ import Password
 class testPassword(unittest.TestCase):
     def test1(self):
         True
+        
+    def test1(self):
+        True    
+    def test1(self):
+        True
+
 
 if __name__ == '__main__':
     unittest.main()
