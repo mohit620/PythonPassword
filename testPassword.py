49aaaa57f5f5e78e7d0b5ab1888074cf057fb4f1
mohit620 <patilmohit25@gmail.com>
2021-01-06 18:12:40 +0530

    master

--- testPassword.py
+++ testPassword.py
@@ -4,11 +4,6 @@ import Password
 class testPassword(unittest.TestCase):
     def test1(self):
         True
-
-    def test1(self):
-        True    
-    def test1(self):
-        True
        
     
 
